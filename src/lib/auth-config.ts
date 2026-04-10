fd107c97f338ce1b4f38a3788352c29d90a78ffc
Luciano Rodriguez <luciano.rodriguez.dev@gmail.com>
2026-04-09 22:56:40 -0300

    usuario creado, iniciado sesion

--- src/lib/auth-config.ts
+++ src/lib/auth-config.ts
@@ -17,7 +17,7 @@ export const auth = betterAuth({
   verification: { modelName: 'verifications' },
   emailAndPassword: {
     enabled: true,
-    disableSignUp: false,
+    disableSignUp: true,
   },
   plugins: [admin()],
   trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL!],
